c7b38b0d04f3f48f7084d1446c4211644094c062
Prakhar Aggarwal <prakharaggarwal87@gmail.com>
2021-02-23 13:28:33 +0530

    Fixed some bugs
    Keeping new values intact in $f2 and $f3

--- Assignment_1/code.asm
+++ Assignment_1/code.asm
@@ -17,6 +17,8 @@ main:
     li.s $f5,0.0 # Will store prev_y
     li.s $f6,2.0 # Constant to divide area by 2
     li.s $f7,0.0 # Constant 0
+    li.s $f8,0.0 # To store y2-y1
+    li.s $f9,0.0 # To store y1^2 + y2^2
     li $t3,0 # To check sign of $f3 and $f5
 
 loop:   beq $t1, $t0, exit
@@ -32,7 +34,7 @@ loop:   beq $t1, $t0, exit
         li $v0,6 # Take float as an input
         syscall
 
-        mov.s $f2,$f0
+        mov.s $f2,$f0 
         li $v0,4
         la $a0,give_y
         syscall
@@ -41,6 +43,21 @@ loop:   beq $t1, $t0, exit
         syscall        
         mov.s $f3,$f0  # Storing y-coord in $f3 
         
+
+
+
+        # li $v0,2  # For debugging
+        # mov.s $f12,$f1
+        # syscall
+        # li $v0,4
+        # la $a0,next_line
+        # syscall
+
+
+
+
+
+
         c.lt.s $f3,$f7,
         bc1t A_          # Current_y < 0
         add $t3,$t3,1 # This will be skipped if f3 is negative
@@ -59,22 +76,32 @@ loop:   beq $t1, $t0, exit
     
 area_mod:
     # area will be (x1-x2)*(y1^2+y2^2)/((y2-y1)*2)
-
-    sub.s $f2,$f2,$f4  # for x1-x2
-    sub.s $f4,$f5,$f3  # for y2-y1
-
-    c.eq.s $f4,$f7  # checking if denom=0
+    # Here, results will be stored in $f4 (step by step)
+    sub.s $f4,$f2,$f4  # for x1-x2
+    sub.s $f8,$f5,$f3  # for y2-y1
+    
+    c.eq.s $f8,$f7  # checking if denom=0
     bc1t D_
 
-    mul.s $f3,$f3,$f3
-    mul.s $f5,$f5,$f5
-    add.s $f3,$f3,$f5  # computing y1^2 + y2^2
-    mul.s $f2,$f2,$f3  # computing (x1-x2)*(y1^2+y2^2)
-    div.s $f2,$f2,$f4  # computing (x1-x2)*(y1^2+y2^2)/((y2-y1))  
-    div.s $f2,$f2,$f6  # computing (x1-x2)*(y1^2+y2^2)/((y2-y1)*2)  
-    abs.s $f2,$f2
-    add.s $f1,$f1,$f2
 
+    # li $v0,2  # For debugging
+    # mov.s $f12,$f1
+    # syscall
+    # li $v0,4
+    # la $a0,next_line
+    # syscall
+
+
+    
+    mul.s $f9,$f3,$f3  # y1^2
+    mul.s $f5,$f5,$f5  # y2^2
+    add.s $f9,$f9,$f5  # y1^2 + y2^2
+    mul.s $f4,$f4,$f9  # computing (x1-x2)*(y1^2+y2^2)
+    div.s $f4,$f4,$f8  # computing (x1-x2)*(y1^2+y2^2)/((y2-y1))  
+    div.s $f4,$f4,$f6  # computing (x1-x2)*(y1^2+y2^2)/((y2-y1)*2)  
+    abs.s $f4,$f4
+    add.s $f1,$f1,$f4
+    
    # li $v0,2
    # mov.s $f12,$f1
    # syscall
@@ -102,13 +129,14 @@ exit:
     # move $a0,$t1
     # syscall
 
-    # li $v0,4
-    # la $a0,msg1
-    # syscall
+    li $v0,4
+    la $a0,area_
+    syscall
 
     li $v0,2
     mov.s $f12,$f1
     syscall
+
     li $v0,4
     la $a0,next_line
     syscall
@@ -119,5 +147,6 @@ exit:
     .data
 msg1: .asciiz "Enter the Number of points(N): "
 next_line: .asciiz "\n"
-give_x: .asciiz "x="
-give_y: .asciiz "y="
+give_x: .asciiz "x = "
+give_y: .asciiz "y = "
+area_: .asciiz "Area = "
